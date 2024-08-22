0fbfdf6e00cb062ac989e3d32f197f0e6336ee20
Hasnain78 <83117993+Hasnain78@users.noreply.github.com>
2024-08-23 01:07:31 +0530

    Update 3_Loops.py

--- 3_Loops.py
+++ 3_Loops.py
@@ -1,1 +1,88 @@
 #1.Write a program to print “Bright IT Career” ten times using for loop
+a='Brighr IT Career'
+for i in range(1,11):
+  print(i,a)
+-----------------------------------------------------------------------
+>>>  1 Brighr IT Career
+      2 Brighr IT Career
+      3 Brighr IT Career
+      4 Brighr IT Career
+      5 Brighr IT Career
+      6 Brighr IT Career
+      7 Brighr IT Career
+      8 Brighr IT Career
+      9 Brighr IT Career
+      10 Brighr IT Career
+
+
+***************************************************************************************************************************************
+
+2. Write a java program to print 1 to 20 numbers using the while loop.
+a=1
+while a<=20:
+  print(a)
+  a+=1
+--------------------------------------------------------------------------
+>>>  1
+      2
+      3
+      4
+      5
+      6
+      7
+      8
+      9
+      10
+      11
+      12
+      13
+      14
+      15
+      16
+      17
+      18
+      19
+      20
+
+**************************************************************************************************************************************
+#3. Program to equal operator and not equal operators 
+a=75
+b=35
+print(a==b)
+print(a!=b)
+---------------------------------------------------------------
+>>>  False
+      True
+
+**************************************************************************************************************************************
+
+4. Write a program to print the odd and even numbers. 
+a=int(input('enter any number '))
+if a%2==0:
+  print(a,' is even number')
+else:
+  print(a,' is odd number')
+  ------------------------------------------------------------
+  >>>  enter any number 91
+        91  is odd number
+
+**************************************************************************************************************************************
+
+5. Write a program to print largest number among three numbers. 
+a=int(input('enter first number '))
+b=int(input('enter second number '))
+c=int(input('enter third number '))
+if a>b and a>c:
+  print(a ,' is largest number')
+elif b>a and b>c:
+  print(b ,' is largest number')
+else:
+  print(c ,' is largest number')
+-------------------------------------------------------------------
+>>>  enter first number 69
+enter second number 59
+enter third number 79
+79  is largest number
+
+**************************************************************************************************************************************
+
